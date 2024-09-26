7ffb43b282e4925fb51c29614e30238096a52795
Cedric Girard <x-dark.github@valinor.fr>
2024-09-26 14:54:59 +0200

    roxctl 4.5.2

---
+++ Formula/r/roxctl.rb
@@ -0,0 +1,26 @@
+class Roxctl < Formula
+  desc "roxctl is the CLI for Stackrox"
+  homepage "https://www.stackrox.io/"
+  url "https://github.com/stackrox/stackrox/archive/refs/tags/4.5.2.tar.gz"
+  sha256 "8ee9b981b890dea660dbacffc01350ef85ad4bfe2e17bf650f8022b272a0c090"
+  license "Apache-2.0"
+  head "https://github.com/stackrox/stackrox.git", branch: "master"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w", output: bin/"roxctl"), "./roxctl"
+
+    generate_completions_from_executable(bin/"roxctl", "completion", base_name: "roxctl")
+  end
+
+  test do
+    output = shell_output("#{bin}/roxctl central whoami 2<&1", 1)
+    assert_match 'ERROR:	obtaining auth information for localhost:8443: retrieving token: no credentials found for localhost:8443, please run "roxctl central login" to obtain credentials', output
+  end
+end
